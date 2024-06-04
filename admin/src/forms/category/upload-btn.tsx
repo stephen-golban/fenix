7e434a2bdb80157ee48cf654548d5827d21314a3
Stefan Golban <stefangolban@Stefans-MacBook-Pro.local>
2024-06-05 02:39:46 +0300

    main: fix category form

--- admin/src/forms/category/upload-btn.tsx
+++ admin/src/forms/category/upload-btn.tsx
@@ -1,11 +1,33 @@
 import { useImageUpload } from '../../lib/hooks';
 
-import { Button, Upload } from 'antd';
+import { Button, Upload, UploadFile } from 'antd';
 import { UploadOutlined } from '@ant-design/icons';
+import { isEmpty } from 'lodash';
+import { useMount } from 'react-use';
 
-const UploadBtn = (props: any) => {
+const UploadBtn: React.FC<any> = props => {
   const { onChange, value, isEditMode = false } = props;
-  const { customRequest, fileList, onRemove, onUploadChange } = useImageUpload({ value, onChange, multiple: false });
+  const fileValue = {
+    uid: value,
+    url: value,
+    name: value,
+  } as UploadFile;
+
+  const handleChange = (arr: UploadFile[]) => {
+    if (isEmpty(arr)) {
+      onChange(undefined);
+    } else {
+      onChange(arr[0].url);
+    }
+  };
+
+  const { customRequest, fileList, setFileList, onRemove, onUploadChange } = useImageUpload({
+    value: [fileValue],
+    onChange: handleChange,
+    multiple: false,
+  });
+
+  useMount(() => setFileList([fileValue]));
 
   return (
     <Upload
--- admin/src/typings/categories.ts
+++ admin/src/typings/categories.ts
@@ -1,3 +1,9 @@
+import { UploadFile } from 'antd';
+
 export type Category = { id: string; title: string; image_url: string };
 
 export type CategoryFormFields = Omit<Category, 'id'>;
+
+export type CategoryFormDefaultFieldValues = Pick<Category, 'title'> & {
+  photos: UploadFile[];
+};
