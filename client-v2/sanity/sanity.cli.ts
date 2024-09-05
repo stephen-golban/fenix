e8d938b46ef74a10eeaf1534c88504845022ec99
Stefan Golban <golban.stephen@gmail.com>
2024-09-05 09:17:43 +0300

    main: saved sanity studiohost

--- client-v2/sanity/sanity.cli.ts
+++ client-v2/sanity/sanity.cli.ts
@@ -3,6 +3,7 @@ import {defineCliConfig} from 'sanity/cli'
 export default defineCliConfig({
   api: {
     projectId: 'tjz04mdk',
-    dataset: 'production'
-  }
+    dataset: 'production',
+  },
+  studioHost: 'fenix-test',
 })
