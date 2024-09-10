eb95c2bb67826d6351e3017a64dc92a59e7a80e3
Stefan Golban <golban.stephen@gmail.com>
2024-09-10 04:10:37 +0300

    main: remove unused assets

--- client-v2/src/sanity/env.ts
+++ client-v2/src/sanity/env.ts
@@ -9,7 +9,7 @@ export const dataset = assertValue(
 );
 
 export const projectId = assertValue(
-  process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || process.env.NEXITY_PROJECT_ID,
+  process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || process.env.SANITY_PROJECT_ID,
   "Missing environment variable: NEXT_PUBLIC_SANITY_PROJECT_ID"
 );
 
